4986e24e7eecaed42b297ad393c44fddf88152ce
peter <peterhamza6@gmail.com>
2023-03-30 18:26:58 +0100

    first commit

---
+++ src/main/java/com/example/fashionblog_restapi/FashionBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashionblog_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionBlogRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionBlogRestApiApplication.class, args);
+    }
+
+}
